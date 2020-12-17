00192929bf27adb93d772c3427027e7a8f78f3ae
Ercan Doganer <caner.erdogan.92@gmail.com>
2020-12-17 19:02:28 +0300

    Day 17

--- week3/day17.py
+++ week3/day17.py
@@ -1,29 +1,38 @@
+from collections import defaultdict
+
 with open('day17.txt') as fp:
     region = fp.read().splitlines()
-    # region = [".#.", "..#", "###"]
-    vectors = [(x, y, z) for x in (-1, 0, 1) \
-                         for y in (-1, 0, 1) \
-                         for z in (-1, 0, 1) if (x, y, z) != (0, 0, 0)]
-    active = set()
+    initial = set()
     for y in range(len(region)):
         for x in range(len(region[y])):
             if region[y][x] == '#':
-                active.add((x, y, 0))
+                initial.add((x, y, 0, 0))
 
-for cycle in range(1, 7):
-    span = []
-    for (x, y, z) in active:
-        for dx, dy, dz in vectors:
-            span.append((x + dx, y + dy, z + dz))
-            
-    updated = set()
-    for coord in span:
-        if coord in active:
-            if 2 <= span.count(coord) <= 3:
-                updated.add(coord)
-        else:
-            if span.count(coord) == 3:
-                updated.add(coord)
-    active = updated.copy()
 
-print(len(active))
+def active_cubes(dim4 = False):
+    active = initial.copy()
+    w_space = (-1, 0, 1) if dim4 else [0]
+    vectors = [(x, y, z, w) for x in (-1, 0, 1) \
+                            for y in (-1, 0, 1) \
+                            for z in (-1, 0, 1) \
+                            for w in w_space if (x, y, z, w) != (0, 0, 0, 0)]
+    for _ in range(1, 7):
+        span = defaultdict(int)
+        for (x, y, z, w) in active:
+            for dx, dy, dz, dw in vectors:
+                span[(x + dx, y + dy, z + dz, w + dw)] += 1
+                
+        update = set()
+        for coord in span:
+            if coord in active:
+                if 2 <= span[coord] <= 3:
+                    update.add(coord)
+            else:
+                if span[coord] == 3:
+                    update.add(coord)
+        active = update.copy()
+    return len(active)
+
+
+print("Active cubes in 3D:", active_cubes(dim4 = False))
+print("Active cubes in 4D:", active_cubes(dim4 = True))
