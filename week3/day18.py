4894d7fa4201e46611bc06900d8df56679ed878e
Ercan Doganer <caner.erdogan.92@gmail.com>
2020-12-18 23:09:55 +0300

    Day 18

--- week3/day18.py
+++ week3/day18.py
@@ -1,9 +1,11 @@
 from operator import add, sub, mul
+from functools import reduce
 
 with open('day18.txt') as fp:
     lines = fp.read().splitlines()
 
-def parse(string):
+
+def parse(string, evaluation):
     parts = string.split(' ')
     elements = []
     substring = ''
@@ -25,16 +27,16 @@ def parse(string):
                 brace -= part.count(')')
                 if brace == 0:
                     substring = ' '.join([substring, part[:-1]])
-                    elements.append(evaluate(parse(substring)))
+                    elements.append(evaluation(parse(substring, evaluation)))
                     substring = ''
                 else:
                     substring = ' '.join([substring, part])
             else:
                 substring = ' '.join([substring, part])
-    return elements#[2, '*', 3, '+', 20]
+    return elements
     
 
-def evaluate(elements):
+def no_precedence(elements):
     left = right = oper = None
     for element in elements:
         if not left:
@@ -51,16 +53,51 @@ def evaluate(elements):
     return left
 
 
-def solve_lines(lines):
+def precedence(elements):
+    left = right = oper = None
+    update = elements.copy()
+    while '+' in update or '-' in update:
+        update = []
+        for element in elements:
+            if not left:
+                left = element
+                update.append(left)
+            elif not oper:
+                if element == '*':
+                    if not update or update[-1] == '*':
+                        update.extend([left, element])
+                    else:
+                        update.append(element)
+                    left = None
+                else:
+                    oper = add if element == '+' else sub
+            elif not right:
+                right = element
+                left = oper(left, right)
+                if not update or update[-1] == '*':
+                    update.append(left)
+                else:
+                    update[-1] = left
+                oper = right = None
+    return reduce(mul, [e for e in update if e != '*'])
+
+
+def solve_lines(lines, evaluation):
     results = []
     for line in lines:
-        results.append(evaluate(parse(line)))
+        results.append(evaluation(parse(line, evaluation)))
     return sum(results)
 
 
-print("Sum of all expressions:", solve_lines(lines))
+print("Without precedence:", solve_lines(lines, evaluation = no_precedence))
+print("With precedence:", solve_lines(lines, evaluation = precedence))
 
 # assert solve_lines(["2 * 3 + (4 * 5)"]) == 26
 # assert solve_lines(["5 + (8 * 3 + 9 + 3 * 4 * 3)"]) == 437
 # assert solve_lines(["5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"]) == 12240
 # assert solve_lines(["((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"]) == 13632
+
+# assert solve_lines(["2 * 3 + (4 * 5)"], precedence) == 46
+# assert solve_lines(["5 + (8 * 3 + 9 + 3 * 4 * 3)"], precedence) == 1445
+# assert solve_lines(["5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"], precedence) == 669060
+# assert solve_lines(["((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"], precedence) == 23340
